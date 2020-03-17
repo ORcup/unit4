840b2995b84d321d6c119c55d74d7904cb6ade0d
try <try@try.com>
2020-03-17 15:16:46 +0800

    fistcommit

--- unit4/tests/test_case1.py
+++ unit4/tests/test_case1.py
@@ -1,5 +1,22 @@
-import unittest
+file_name='word_count.txt'
+line_count=0
+word_count=0
+character_count=0
+with open(file_name,'r',encoding='utf-8') as f:
+    for line in f:
+        word=line.split()
+        line_count+=1
+        word_count+=len(word)
+        character_count+=len(line)
+print('行数：',line_count)
+print('单词数：',word_count)
+print('字母数：', character_count)
 
-class TestClass1(unittest.TestCase):
-    def test_case1(self):
-        self.assertEqual(True, True)
+def positive():
+    if line_count > 0 and word_count > 0 and character_count > 0:
+        return 1
+    else:
+        return 0
+
+def test_positve():
+    assert positive() == 1
